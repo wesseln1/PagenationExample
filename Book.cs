e48207ddb938959bb51bb3eb0cc6ded47a2dd348
Adam <adam.e.sheaffer@gmail.com>
2019-11-26 09:06:00 -0600

    add ids

--- Book.cs
+++ Book.cs
@@ -4,6 +4,7 @@ namespace GenericsExample
 {
     public class Book
     {
+        public int Id { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
     }
--- DVD.cs
+++ DVD.cs
@@ -4,6 +4,7 @@ namespace GenericsExample
 {
     public class DVD
     {
+        public int Id { get; set; }
         public string Title { get; set; }
         public string Genre { get; set; }
     }
